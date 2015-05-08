a1fe20cd5a36da340f9d2f31ecafbb37afa164a4
yohananjr13 <yohananjr13@gmail.com>
2015-05-08 17:29:08 +0700

    add list data

--- app/src/main/java/studio/plus/horizontalscroll/activities/SecondActivity.java
+++ app/src/main/java/studio/plus/horizontalscroll/activities/SecondActivity.java
@@ -1,7 +1,7 @@
 package studio.plus.horizontalscroll.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,11 +9,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import studio.plus.horizontalscroll.R;
 
-
 public class SecondActivity extends ActionBarActivity {
+
+    final List<String> list = new ArrayList<String>(){{
+        add("10.000");
+        add("20.000");
+        add("50.000");
+        add("100.000");
+        add("200.000");
+        add("500.000");
+    }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +33,36 @@ public class SecondActivity extends ActionBarActivity {
         setContentView(R.layout.activity_second);
 
         LayoutInflater inflater = getLayoutInflater();
+        LinearLayout wrapper = (LinearLayout) findViewById(R.id.group_wrapper);
+        wrapper.removeAllViews();
 
 
-        for(int i =0; i < 5; i++){
-            View view = inflater.inflate(R.layout.custom_single_group, null);
+        for (int i = 0; i < list.size(); i++) {
+            View view = inflater.inflate(R.layout.custom_single_group, (LinearLayout) findViewById(R.id.group_wrapper), false);
+            if (i == 4) {
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+                params.setMargins(0, 0, 0, 0);
+                view.setLayoutParams(params);
+            }
+
+            final int position = i;
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), list.get(position), Toast.LENGTH_SHORT).show();
+                }
+            });
 
             TextView title = (TextView) view.findViewById(R.id.title);
             TextView realValue = (TextView) view.findViewById(R.id.real_value);
             TextView oldValue = (TextView) view.findViewById(R.id.old_value);
-            LinearLayout wrapper = (LinearLayout) findViewById(R.id.group_wrapper);
+
             Log.d("logging", i + "");
             title.setText("title " + i);
-            realValue.setText("realValue " + i);
+            realValue.setText(list.get(i));
             oldValue.setText("oldValue " + i);
 
-            wrapper.addView(view, 0);
+            wrapper.addView(view, i);
         }
     }
 
